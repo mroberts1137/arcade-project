7af3cc670bdf24f14dfc4a919a10f031e9c38b5e
mroberts1137 <mroberts1137@gmail.com>
2023-12-02 11:19:39 -0500

    Class constructors now take props object as argument

--- javascript/main.js
+++ javascript/main.js
@@ -1,15 +1,19 @@
 class player {
-  constructor(position) {
-    this.position = position;
+  constructor(props) {
+    this.x = props.x;
+    this.y = props.y;
   }
 
   move() {}
 }
 
 class alien {
-  constructor(position) {
-    this.position = position;
+  constructor(props) {
+    this.x = props.x;
+    this.y = props.y;
   }
 
   move() {}
 }
+
+player = new player({ x: 10, y: 10 });
